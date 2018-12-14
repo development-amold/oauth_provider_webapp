7637f570f9967db0b13a7b442f3b817e52cb24b7
Amol Dumbre <amol.dumbre@gslab.com>
2018-12-14 16:28:23 +0530

    Added return URL using cookies

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,11 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  include ApplicationHelper
   # GET /resource/sign_in
   def new
+    puts "---ENCRYPTION------------#{encrypt_str("amol")}------REQ---#{request.original_url}---"
+    @client_params = encrypt_str(params)
     if params.has_key?("client_id")
       cookies.signed[:continue] = params #-----return url
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	def encrypt_str payload
+		len   = ActiveSupport::MessageEncryptor.key_len
+		salt  = SecureRandom.random_bytes(len)
+		key   = ActiveSupport::KeyGenerator.new('uri').generate_key(salt, len) 
+		crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets[:crypt_key])
+		encrypted_data = crypt.encrypt_and_sign(payload)                   
+		 		
+	end
+
+	def decrypt_str payload
+		crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets[:crypt_key])   		
+		crypt.decrypt_and_verify(payload)
+	end	
 end
